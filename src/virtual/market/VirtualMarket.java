97e7e9aee793eca21085814330a187a2cdb93111
Tharikh <tharikhlegend@gmail.com>
2020-11-16 16:51:21 +0530

    signin page

---
+++ src/virtual/market/VirtualMarket.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtual.market;
+import Frames.SignIn;
+/**
+ *
+ * @author tharikh
+ */
+public class VirtualMarket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new SignIn().setVisible(true);
+    }
+    
+}
